9e0fef8921215fcbada9fd347f29003c5f0a5791
Tiago de Paula <tiagodepalves@gmail.com>
2020-11-12 10:14:29 -0300

    DOC: Numba mocks

--- lib/nb.py
+++ lib/nb.py
@@ -1,8 +1,14 @@
+"""
+Tratamento da biblioteca Numba, em caso de não ser encontrada.
+"""
+# funcionamento correto da biblioteca
 try:
     from numba import jit as nb_jit, prange
 
     USANDO_NUMBA = True
 
+# biblioteca inexistente, cria funções com mesma API, mas que não
+# fazem nada no código, apenas continuam a execução e Python puro
 except ImportError:
     def nb_jit(*args, **kwargs):
         return lambda func: func
@@ -11,6 +17,10 @@ except ImportError:
     USANDO_NUMBA = False
 
 
+
 def jit(signature: str, *, parallel=False, locals={}):
-    return nb_jit(signature, locals=locals, parallel=parallel,
-        nopython=True, nogil=True, cache=True, fastmath=True, error_model='numpy')
+    """
+    Wrapper da ``numba.jit``, com opções padrões diferentes.
+    """
+    return nb_jit(signature, locals=locals, parallel=parallel, error_model='numpy',
+                  nopython=True, nogil=True, cache=True, fastmath=True)
