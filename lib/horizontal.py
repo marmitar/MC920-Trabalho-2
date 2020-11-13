d25f4f8e51e7f39117b3c1693efa58e4949e0120
Tiago de Paula <tiagodepalves@gmail.com>
2020-11-12 21:39:53 -0300

    DOC: unidir e alternado

--- lib/horizontal.py
+++ lib/horizontal.py
@@ -35,7 +35,7 @@ def varredura_unidirecional(img: Image, dist: ErrorDist) -> Image:
     # deslocamento em `x` do início da dist.
     dW = (tW - 1) // 2
 
-    # imagem em ponto flutuante, para usar os erros
+    # imagem em ponto flutuante, para não arredondar erros
     img = img.astype(np.float32)
     # imagem resultante
     res = np.empty((H, W), dtype=np.uint8)
@@ -43,6 +43,7 @@ def varredura_unidirecional(img: Image, dist: ErrorDist) -> Image:
     # aplicação em cada pixel
     for y in range(H):
         for x in range(W):
+            # meio ton simples
             intensidade = img[y, x]
             if intensidade < 128.0:
                 res[y, x] = 0
@@ -51,11 +52,13 @@ def varredura_unidirecional(img: Image, dist: ErrorDist) -> Image:
                 res[y, x] = 1
                 valor = 255.0
 
+            # carregamento do erro
             erro = intensidade - valor
             for i in range(tH):
                 yi = y + i
                 for j in range(tW):
                     xj = x + j - dW
+                    # cuidado com acesso out-of-bounds
                     if 0 <= yi < H and 0 <= xj < W:
                         img[yi, xj] += dist[i, j] * erro
     return res
@@ -66,18 +69,23 @@ def varredura_unidirecional(img: Image, dist: ErrorDist) -> Image:
 
 @jit("uint8[:,::1](uint8[:,::1], float32[:,::1])")
 def varredura_alternada(img: Image, dist: ErrorDist) -> Image:
+    # dimensões da imagem
     H, W = img.shape
-
+    # dimensões da distribuição de erros
     tH, tW = dist.shape
-    dH, dW = (tH - 1)//2, (tW - 1)//2
+    # deslocamento da máscara de dist. erros
+    dW = (tW - 1) // 2
 
+    # imagem em ponto flutuante
     img = img.astype(np.float32)
+    # imagem resultante
     res = np.empty((H, W), dtype=np.uint8)
 
     for ym in range((H + 1)//2):
+        # aplicação nas linhas pares
         y = 2 * ym
-
         for x in range(W):
+            # semelhante ao unidirecional
             intensidade = img[y, x]
             if intensidade < 128.0:
                 res[y, x] = 0
@@ -86,20 +94,23 @@ def varredura_alternada(img: Image, dist: ErrorDist) -> Image:
                 res[y, x] = 1
                 valor = 255.0
 
+            # carregamento do erro
             erro = intensidade - valor
             for i in range(tH):
-                yi = y + i - dH
+                yi = y + i
                 for j in range(tW):
                     xj = x + j - dW
                     if 0 <= yi < H and 0 <= xj < W:
                         img[yi, xj] += dist[i, j] * erro
 
-        y += 1
-        if y == H:
+        if y + 1 == H:
             break
-
+        # aplicação nas linhas ímpares
+        y += 1
         for xm in range(W):
+            # ordem invertida na linha
             x = W - 1 - xm
+
             intensidade = img[y, x]
             if intensidade < 128.0:
                 res[y, x] = 0
@@ -108,12 +119,13 @@ def varredura_alternada(img: Image, dist: ErrorDist) -> Image:
                 res[y, x] = 1
                 valor = 255.0
 
+            # carregamento do erro
             erro = intensidade - valor
             for i in range(tH):
-                yi = y + i - dH
-                for jm in range(tW):
-                    j = tW - 1 - jm
-                    xj = x + j - dW
+                yi = y + i
+                for j in range(tW):
+                    # aplicação invertida da máscara de erros
+                    xj = x + dW - j
                     if 0 <= yi < H and 0 <= xj < W:
                         img[yi, xj] += dist[i, j] * erro
     return res
