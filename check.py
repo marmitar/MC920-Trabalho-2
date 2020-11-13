e12af8e2f2ed1732bf74fb2e8cf697d4ad27e5ea
Tiago de Paula <tiagodepalves@gmail.com>
2020-11-13 08:07:02 -0300

    Métricas de qualidade

---
+++ check.py
@@ -0,0 +1,42 @@
+import cv2, sys
+import numpy as np
+
+
+def sumsq(x):
+    return np.sum(np.square(x))
+
+
+def RMSE(f, g):
+    return np.sqrt(sumsq(f - g) / f.size)
+
+def SNR(f, g):
+    return 10 * np.log10(sumsq(f) / sumsq(f - g))
+
+def PSNR(f, g):
+    return 20 * np.log10(255 / RMSE(f, g))
+
+def cov(f, g):
+    mf, mg = np.mean(f), np.mean(g)
+    return np.sum((f - mf) * (g - mg)) / f.size
+
+def corr(f, g):
+    mf, mg = np.mean(f), np.mean(g)
+    return np.sum((f - mf) * (g - mg)) / np.sqrt(sumsq(f - mf) * sumsq(g - mg))
+
+def jaccard(f, g):
+    return np.count_nonzero(f == g) / f.size
+
+
+DIFS = [RMSE, SNR, PSNR, cov, corr, jaccard]
+
+
+_, img, dist, varredura = sys.argv
+
+f = cv2.imread(f'imagens/{img}.png')
+g = cv2.imread(f'build/{varredura}/{dist}_{img}.png')
+
+if f is None or g is None:
+    raise ValueError(f'não foi possível ler imagens', (img, dist, varredura))
+
+for fun in DIFS:
+    print(fun.__name__, fun(f, g))
