6d1465b58c1418325f12500b2efc7849147b54b6
Tiago de Paula <tiagodepalves@gmail.com>
2020-11-13 10:35:45 -0300

    Novo run.sh

--- run.sh
+++ run.sh
@@ -10,6 +10,9 @@ dists=(floyd stevenson burkes sierra stucki jarvis)
 for v in ${varredura[@]}; do
     mkdir -p $build/$v
 done
+for d in ${dists[@]}; do
+    mkdir -p $build/$d
+done
 
 proc=0
 # pontilhados
@@ -30,5 +33,15 @@ done
 # espera processos encerrarem
 for pid in ${pids[@]}; do
     wait $pid
+done
+
+# copia em outras pastas
+for file in $(ls imagens/*.png); do
+    out=$(basename $file)
+    for v in ${varredura[@]}; do
+        for d in ${dists[@]}; do
+            cp $build/$v/$d\_$out $build/$d/$v\_$out
+        done
+    done
 done
 echo Done!
