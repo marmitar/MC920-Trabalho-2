18d66b1ff568660eef592a76f9684d3dd6fc39cf
Tiago de Paula <tiagodepalves@gmail.com>
2020-11-13 00:05:53 -0300

    Execução das imagens

---
+++ run.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/bash
+
+prog="python3 main.py $@"
+build=build
+
+varredura=(unidirecional alternada hilbert espiral)
+dists=(floyd stevenson burkes sierra stucki jarvis)
+
+# pastas de saída
+for v in ${varredura[@]}; do
+    mkdir -p $build/$v
+done
+
+proc=0
+# pontilhados
+for file in $(ls imagens/*.png); do
+    out=$(basename $file)
+    echo -n $out ...
+
+    for v in ${varredura[@]}; do
+        for d in ${dists[@]}; do
+            $prog -o $build/$v/$d_$out $file -v $v -d $d &
+            pids[${proc}]=$!
+            proc=$((proc+1))
+        done
+    done
+    echo ' 'waiting
+done
+
+# espera processos encerrarem
+for pid in ${pids[@]}; do
+    wait $pid
+done
+echo Done!
