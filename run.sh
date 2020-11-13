f0a85ca976d0157425f90349df631e8f380c9ce8
Tiago de Paula <tiagodepalves@gmail.com>
2020-11-13 11:33:37 -0300

    Novo build

--- run.sh
+++ run.sh
@@ -5,24 +5,28 @@ build=build
 
 varredura=(unidirecional alternada hilbert espiral)
 dists=(floyd stevenson burkes sierra stucki jarvis)
+imagens=$(ls imagens/*.png)
 
 # pastas de saída
 for v in ${varredura[@]}; do
-    mkdir -p $build/$v
-done
-for d in ${dists[@]}; do
-    mkdir -p $build/$d
+    for d in ${dists[@]}; do
+        mkdir -p $build/$v/$d
+        mkdir -p $build/grayscale/$v/$d
+    done
 done
 
 proc=0
 # pontilhados
-for file in $(ls imagens/*.png); do
+for file in ${imagens[@]}; do
     out=$(basename $file)
     echo -n $out ...
 
     for v in ${varredura[@]}; do
         for d in ${dists[@]}; do
-            $prog -o $build/$v/$d\_$out $file -v $v -d $d &
+            $prog -o $build/$v/$d/$out $file -v $v -d $d &
+            pids[${proc}]=$!
+            proc=$((proc+1))
+            $prog -o $build/grayscale/$v/$d/$out $file -v $v -d $d -g &
             pids[${proc}]=$!
             proc=$((proc+1))
         done
@@ -36,11 +40,18 @@ for pid in ${pids[@]}; do
 done
 
 # copia em outras pastas
-for file in $(ls imagens/*.png); do
-    out=$(basename $file)
+for img in ${imagens[@]}; do
+    out=$(basename $img)
+    m="${out%.*}"
     for v in ${varredura[@]}; do
         for d in ${dists[@]}; do
-            cp $build/$v/$d\_$out $build/$d/$v\_$out
+            mkdir -p $build/$d/$v
+            mkdir -p $build/$d/$m
+            mkdir -p $build/$v/$m
+
+            cp $build/$v/$d/$out $build/$d/$v/$m.png
+            cp $build/$v/$d/$out $build/$d/$m/$v.png
+            cp $build/$v/$d/$out $build/$v/$m/$d.png
         done
     done
 done
